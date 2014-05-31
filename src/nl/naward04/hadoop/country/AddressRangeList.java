850d043cef2d3770359a1d917d54362c71aed0d5
naward <naward@awardvm>
2014-05-31 13:27:00 +0200

    Lookup afgemaakt, javadoc geschreven en een JUnit-testje gemaakt. Het werkt!

--- src/nl/naward04/hadoop/country/AddressRangeList.java
+++ src/nl/naward04/hadoop/country/AddressRangeList.java
@@ -4,17 +4,32 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
+/**
+ *  Singleton-class that populates and maintains a list of IPv4-address ranges (AddressRange-class) and allows searching through the list using a 
+ *  binary search. Requires a ;-delimited iplist.csv file in the path containing non-overlapping sorted IP-ranges.
+ * 
+ * @author Marc Hulsebosch
+ *
+ */
 public class AddressRangeList {
 	
+	//The singleton instance
 	private static AddressRangeList instance;
 	
-	private AddressRange[] list;
+	/**
+	 * The array containing the list of AddressRanges. Is sorted, as the ordering from the sorted CSV-file is maintained.
+	 */
+	protected AddressRange[] list;
 	
+	//Private constructor that populates the list.
 	private AddressRangeList(){
 		populate();
 	}
 	
+	/**
+	 * Method for retrieving the singleton-instance of this class.
+	 * @return The instance of this class.
+	 */
 	public static AddressRangeList getInstance(){
 		if (instance == null){
 			instance = new AddressRangeList();
@@ -22,6 +37,9 @@ public class AddressRangeList {
 		return instance;
 	}
 	
+	/**
+	 * Reads from iplist.csv and places all lines into the list in the form of a AddressRange.
+	 */
 	private void populate(){
 		Scanner s = null;
 		File f = new File("iplist.csv");
@@ -40,6 +58,15 @@ public class AddressRangeList {
 		s.close();
 	}
 	
+	
+	/**
+	 * Returns the country to which the range containing the address has been assigned as a two-letter string.
+	 * Uses binary search to find the desired range or returns "Unknown". The latter happens if the IP-range is not present in MaxMind's database, or if the number provided doesn't represent a valid IP-address.
+	 * For example, the webserver of utwente.nl:
+	 * getCountry(130.89.3.249) would return "NL".
+	 * @param address The address to be found, as a long.
+	 * @return The country the IP-address belongs to, or "Unknown".
+	 */
 	public String getCountry(long address){
 		int lower = 0;
 		int upper = list.length-1;
@@ -47,21 +74,28 @@ public class AddressRangeList {
 		int middle = (lower + upper)/2;
 		
 		while( lower <= upper ){
-	      if ( list[middle].compareTo(address) > 0 )
-	        lower = middle + 1;    
-	      else if ( list[middle].contains(address)) 
-	      {
-	       ret = list[middle].getCountry();
-	       break;
-	      }
-	      else {
-	    	  upper = middle - 1;
-	    	  middle = (lower + upper)/2;
+			//If the range starts at a higher number than the address (list[middle].getFrom() > address)
+	      if ( list[middle].compareTo(address) > 0 ) {
+	        lower = middle + 1;
+	      	middle = (lower + upper)/2;
+	      //If the range ends at a lower  number than the address (list[middle].getTo() < address)
+		  } else if ( list[middle].compareTo(address) < 0) { 
+	       upper = middle -1;
+	       middle = (lower + upper)/2;
+	       //If both of those are false, the IP should be in this range
+		  } else{
+	    	 ret = list[middle].getCountry();
+	    	 break;
 	      }
 		}
 		return ret;
 	}
 	
+	/**
+	 * Method for converting an IP-address to a long.
+	 * @param address The address to be converted
+	 * @return The IP-address as a long.
+	 */
 	public static long convertAddressToLong(String address){
 		String[] addr = address.split("\\.");
 		long ret=0;
--- src/nl/naward04/junit/TestAddressLookup.java
+++ src/nl/naward04/junit/TestAddressLookup.java
@@ -10,13 +10,30 @@ import static org.junit.Assert.assertEquals;
 public class TestAddressLookup {
 	
 	@Test
-	public void lookup(){
+	public void testValid(){
 		String address = "130.89.128.153";
 		Long longAddress = AddressRangeList.convertAddressToLong(address);
 		AddressRangeList list = AddressRangeList.getInstance();
 		String country = list.getCountry(longAddress);
-		assertEquals(country,"NL");
+		assertEquals("NL",country);
 		System.out.println("Land gevonden: " + country); 
 	}
 
+	@Test
+	public void testMissing(){
+		String address = "24.152.100.100";
+		Long longAddress = AddressRangeList.convertAddressToLong(address);
+		AddressRangeList list = AddressRangeList.getInstance();
+		String country = list.getCountry(longAddress);
+		assertEquals("Unknown",country);
+	}
+	
+	@Test
+	public void testOutOfRange(){
+		String address = "257.258.300.199";
+		Long longAddress = AddressRangeList.convertAddressToLong(address);
+		AddressRangeList list = AddressRangeList.getInstance();
+		String country = list.getCountry(longAddress);
+		assertEquals("Unknown",country);
+	}
 }
